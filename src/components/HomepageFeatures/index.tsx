cf05d2a582bba3f4fe76e61fab71425740d2da9b
Angela94Per <angela@granparamo.com>
2024-08-14 16:45:44 -0500

    Update website (#2)
    * Update
    
    * website update, kavoku video, github link removal
    
    * Update version
    
    * Feacture
    
    * prueba
    
    * Resolved Components
    
    * Image

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,7 +10,7 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: '1.',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -20,7 +20,7 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '2...',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -30,7 +30,7 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: '3',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
